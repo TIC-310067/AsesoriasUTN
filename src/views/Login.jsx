5452a013ec256e587a4824ec1e64fdb00d8daeb1
TIC-310067 <tic-310067@utnay.edu.mx>
2026-03-26 09:26:10 -0700

    feat: estructura inicial del proyecto

---
+++ src/views/Login.jsx
@@ -0,0 +1,36 @@
+import { useEffect } from "react";
+
+function Login() {
+
+    useEffect(() => {
+    document.title = "Login";
+  }, []);
+    
+  {/* Con esta estructura se comenta */}
+
+  return (
+    <div> {/* Inicio HTML */}
+      <h1>Login</h1>
+
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+        <legend className="fieldset-legend">Login</legend>
+
+        <label className="label">Email</label>
+        <input type="email" className="input" placeholder="Email" />
+
+        <label className="label">Password</label>
+        <input type="password" className="input" placeholder="Password" />
+
+        <button className="btn btn-neutral mt-4">Login</button>
+        </fieldset>
+    
+    
+    
+    
+    
+    </div>
+
+  )
+}
+
+export default Login
